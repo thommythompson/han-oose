f6729e1a9367b1fdce71bcbe46832ce086699681
thommythompson <thomashofman2@gmail.com>
2022-10-17 20:21:46 +0200

    basic html prototype

--- ICDETool/ICDETool/Program.cs
+++ ICDETool/ICDETool/Program.cs
@@ -28,7 +28,7 @@ public class Program
 
         app.MapControllerRoute(
             name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
+            pattern: "{controller=Template}/{action=Index}/{id?}");
 
         app.Run();
     }
