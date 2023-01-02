c838238d71403892907371a426ab1220524c6d32
thommythompson <thomashofman2@gmail.com>
2023-01-02 11:56:08 +0100

    csv exporter implementation

--- src/HAN.ICDETool.ExporterService/ExportStrategies/CsvExporterStrategy.cs
+++ src/HAN.ICDETool.ExporterService/ExportStrategies/CsvExporterStrategy.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace HAN.ICDETool.ExporterService.ExportStrategies;
 
 public class CsvExporterStrategy : ExporterStrategy
@@ -9,6 +11,18 @@ public class CsvExporterStrategy : ExporterStrategy
     public override string ConvertStringListToFile(IList<string> exportData, string exportDirectory)
     {
         string fullPath = exportDirectory + Guid.NewGuid() + ".csv";
+
+        var stringBuilder = new StringBuilder();
+
+        int x = 0;
+        foreach (var line in exportData)
+        {
+            stringBuilder.AppendLine($"{x}, {line}");
+            x++;
+        }
+
+        System.IO.File.WriteAllText(fullPath, stringBuilder.ToString());
+        
         return fullPath;
     }
 }
